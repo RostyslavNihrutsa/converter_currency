0f4dfb5168069fa836ac8dbffdccfa339a7c1faf
RostyslavNihrutsa <82962343+RostyslavNihrutsa@users.noreply.github.com>
2023-03-27 01:08:47 +0300

    fixed useEffect and className error

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ function Loader() {
     <div className="flex h-full items-center justify-center" role="status">
       <svg
         aria-hidden="true"
-        class="fill-gradient mr-2 h-12 w-12 animate-spin fill-blue-600 text-gray-200 dark:text-gray-300"
+        className="fill-gradient mr-2 h-12 w-12 animate-spin fill-blue-600 text-gray-200 dark:text-gray-300"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
@@ -17,7 +17,7 @@ function Loader() {
           fill="currentFill"
         />
       </svg>
-      <span class="sr-only">Loading...</span>
+      <span className="sr-only">Loading...</span>
     </div>
   );
 }
